dcf019ccd805b3ca54ad3a0d5a153fb1f5d74852
codingphil <phlonsing@gmail.com>
2020-01-28 22:18:23 +0100

    set focus to visible view (project or package explorer)

--- dev.lonsing.eclipse.plugins.gotoproject/src/dev/lonsing/eclipse/plugins/gotoproject/handlers/GoToProjectHandler.java
+++ dev.lonsing.eclipse.plugins.gotoproject/src/dev/lonsing/eclipse/plugins/gotoproject/handlers/GoToProjectHandler.java
@@ -1,7 +1,7 @@
 package dev.lonsing.eclipse.plugins.gotoproject.handlers;
 
 import java.util.Arrays;
-import java.util.Optional;
+import java.util.Objects;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
@@ -15,8 +15,9 @@ import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IViewPart;
-import org.eclipse.ui.IViewReference;
+import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import dev.lonsing.eclipse.plugins.gotoproject.Messages;
@@ -32,22 +33,47 @@ public class GoToProjectHandler extends AbstractHandler {
     GoToProjectSelectionDialog selectionDialog = new GoToProjectSelectionDialog(window.getShell());
     if (selectionDialog.open() == IDialogConstants.OK_ID) {
       IProject selectedProject = selectionDialog.getSelectedProject();
-      IViewReference[] viewRefs = window.getActivePage().getViewReferences();
-      selectProjectInView(selectedProject, IPageLayout.ID_PROJECT_EXPLORER, viewRefs);
-      IJavaProject selectedJavaProject = projectToJavaProject(selectedProject);
-      selectProjectInView(selectedJavaProject != null ? selectedJavaProject : selectedProject, JavaUI.ID_PACKAGES,
-          viewRefs);
+      selectProjectInViews(window, selectedProject);
     }
     return null;
   }
 
-  private void selectProjectInView(Object selectedProject, String viewId, IViewReference[] viewRefs) {
-    Optional<IViewReference> projectViewRef = Arrays.stream(viewRefs).filter(v -> v.getId().equals(viewId)).findAny();
-    if (projectViewRef.isPresent()) {
-      IViewPart projectViewPart = projectViewRef.get().getView(false);
-      if (projectViewPart != null && projectViewPart.getViewSite() != null) {
-        projectViewPart.getViewSite().getSelectionProvider().setSelection(new StructuredSelection(selectedProject));
+  private void selectProjectInViews(IWorkbenchWindow window, IProject selectedProject) throws ExecutionException {
+    IWorkbenchPage activePage = window.getActivePage();
+    IViewPart projectExplorerViewPart = getViewPart(activePage, IPageLayout.ID_PROJECT_EXPLORER);
+    IViewPart packageExplorerViewPart = getViewPart(activePage, JavaUI.ID_PACKAGES);
+    if (!activePage.isPartVisible(projectExplorerViewPart) && !activePage.isPartVisible(packageExplorerViewPart)) {
+      if (packageExplorerViewPart != null) {
+        try {
+          packageExplorerViewPart = activePage.showView(JavaUI.ID_PACKAGES);
+        } catch (PartInitException e) {
+          throw new ExecutionException("Failed to show Package Explorer", e);
+        }
+      } else {
+        try {
+          projectExplorerViewPart = activePage.showView(IPageLayout.ID_PROJECT_EXPLORER);
+        } catch (PartInitException e) {
+          throw new ExecutionException("Failed to show Project Explorer", e);
+        }
       }
+    }
+    selectProjectInView(selectedProject, projectExplorerViewPart, activePage);
+    IJavaProject selectedJavaProject = projectToJavaProject(selectedProject);
+    selectProjectInView(selectedJavaProject != null ? selectedJavaProject : selectedProject, packageExplorerViewPart,
+        activePage);
+  }
+
+  private IViewPart getViewPart(IWorkbenchPage activePage, String viewId) {
+    return Arrays.stream(activePage.getViewReferences()).filter(r -> viewId.equals(r.getId()))
+        .map(r -> r.getView(false)).filter(Objects::nonNull).findAny().orElse(null);
+  }
+
+  private void selectProjectInView(Object selectedProject, IViewPart viewPart, IWorkbenchPage activePage) {
+    if (viewPart != null && viewPart.getViewSite() != null) {
+      if (activePage.isPartVisible(viewPart)) {
+        viewPart.setFocus();
+      }
+      viewPart.getViewSite().getSelectionProvider().setSelection(new StructuredSelection(selectedProject));
     }
   }
 
